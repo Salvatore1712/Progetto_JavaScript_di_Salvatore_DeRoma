63ce2c413eee2b06f5a327b719dc517f3517f776
Salvatore De Roma <sdrsalvatore@gmail.com>
2026-03-19 17:55:25 +0300

    update fixed

--- js/darkLightMode.js
+++ js/darkLightMode.js
@@ -3,23 +3,27 @@
     //selezione parent per inserimento
     const darkSpace = document.querySelector("#dark-mode");
     let boxNumero = document.querySelector(".box-numero")
+    const contNumero = document.querySelector(".contatore")
+
     //creazione bottone
     const btnDarkMode = document.createElement("button");
     btnDarkMode.classList = "dark-button"
-    btnDarkMode.textContent = "DARK 🌛";
+    btnDarkMode.textContent = "LIGHT";
 
 
     btnDarkMode.addEventListener("click", (event) => {
         let eleCliccato = event.target;
-        if (eleCliccato.textContent === "DARK 🌛") {
-            btnDarkMode.textContent = "LIGHT ☀️";
+        if (eleCliccato.textContent === "LIGHT") {
+            btnDarkMode.textContent = "DARK";
             document.body.classList.add("dark-theme");
             boxNumero.classList.add("dark-display");
+            contNumero.classList.toggle("dark-display")
 
-        } else if (eleCliccato.textContent === "LIGHT ☀️") {
-            btnDarkMode.textContent = "DARK 🌛";
+        } else if (eleCliccato.textContent === "DARK") {
+            btnDarkMode.textContent = "LIGHT";
             document.body.classList.remove("dark-theme");
             boxNumero.classList.remove("dark-display");
+            contNumero.classList.toggle("dark-display")
 
         }
             
